2e6848ad0a16c869ca99f7ffea4117b10e18b724
Matthew Thorley <mthorley@nav.com>
2018-08-11 00:01:03 -0600

    Bump target version

--- bible-ref.rb
+++ bible-ref.rb
@@ -4,8 +4,8 @@
 class BibleRef < Formula
   desc "A perl script to look up Bible references and search for verses."
   homepage ""
-  url "https://github.com/padwasabimasala/bible-ref/archive/v1.0.0.tar.gz"
-  sha256 "e37139ef2f355709276dad25730068c1ff0a7ddaca80b972ffa74ac3eb321a80"
+  url "https://github.com/padwasabimasala/bible-ref/archive/v1.1.0.tar.gz"
+  sha256 "9df4f1712ad4c7407a831ea8e3deb50c52330eafe88a3d1605f0d218fe1ec643"
 
   def install
     bin.install "bible-ref"
